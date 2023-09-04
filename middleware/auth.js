97764cf760754edba9ac4ca6ee7e44cc2d26f1e1
ALTGAME <krgpsh13@gmail.com>
2023-09-04 16:48:13 +0530

    update

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,11 +4,12 @@ require("dotenv").config();
 const authenticate = (req, res, next) => {
 
     try {
-        const token = req.header('Authorization');
+      const token = req.header('Authorization');
         console.log(token);
         const user = jwt.verify(token, process.env.TOKEN_SECRET);
         console.log('userID >>>> ', user.userId)
         User.findByPk(user.userId).then(user => {
+          
 
             req.user = user; 
             next();
